7793054c2c2188f7fdddacdd1c6d52529450c545
Sandesh Dhakal <120581258+Giggity14@users.noreply.github.com>
2025-07-15 17:35:48 +0545

    Delete Inventory_Management_System-main directory

--- Inventory_Management_System-main/js/splash.js
+++
@@ -1,6 +0,0 @@
-function showLoading() {
-  document.getElementById("overlay").style.display = "block";
-  setTimeout(function () {
-    document.getElementById("overlay").style.display = "none";
-  }, 2000);
-}
--- Inventory_Management_System-main/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // start session
-session_unset(); // unset all session variables
-session_destroy(); // destroy session
-header("Location: ../html/login.php"); 
-exit();
-?>
